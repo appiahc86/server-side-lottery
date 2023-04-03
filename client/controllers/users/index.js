980d512f08bc026ea75933cb26932032eb0facd5
Appiah Collins <appiahc86@gmail.com>
2023-04-03 11:35:17 +0000

    added pagination to tickets

--- client/controllers/users/index.js
+++ client/controllers/users/index.js
@@ -6,20 +6,37 @@ const userIndexController = {
     viewProfile: async (req, res) => {
         return res.status(400).send('Bad request')
     },
-
+ 
 
     //View Tickets
     viewTickets: async (req, res) => {
+    
         try {
+           
+            const page = req.query.page || 1;
+            const pageSize = req.query.pageSize || 10;
+
             const data = await db.select( 'tickets.id','tickets.numbers', 'tickets.ticketDate',
                     'tickets.ticketStatus', 'tickets.amount',
-                'tickets.payable', 'winners.amountWon')
+                'tickets.payable', 'winners.amountWon', 'machineNumbers.numbers as winningNumbers')
                 .from('tickets')
                 .leftJoin('winners', 'winners.ticketId', 'tickets.id')
+                .leftJoin('machineNumbers', 'machineNumbers.drawDate', 'tickets.ticketDate')
                 .where('tickets.userId', req.user.id)
-                .orderBy('tickets.id', 'desc').limit(20);
+                .limit(pageSize)
+                .offset((page - 1) * pageSize)
+                .orderBy('tickets.id', 'desc')
 
-            res.status(200).send({data});
+                const total = await db('tickets')
+                .where('tickets.userId', req.user.id)
+                .count('* as total')
+
+            res.status(200).send({
+                data,
+                page,
+                pageSize,
+                totalRecords: total[0].total
+            });
 
         }catch (e) {
             logger.error('client, user index viewTickets');
